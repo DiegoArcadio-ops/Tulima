0264c64a9de6296c80cb36295b15604ff8121031
Alan Parra <gohanEnd89@outlook.com>
2026-06-25 17:30:34 -0600

    ajuste de sesion 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import MainLayout from "./layouts/MainLayout";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import Home from "./pages/home";
 import Hoteles from "./pages/Hoteles";
 import Restaurantes from "./pages/Restaurantes";
@@ -8,50 +9,54 @@ import Login from "./pages/login";
 import Admin from "./pages/admin";
 import Registro from "./pages/registro";
 import DashboardProveedor from "./pages/DashboardProveedor";
+import MainLayout from "./layouts/MainLayout";
 import RutaProtegidaAdmin from "./components/RutaProtegidaAdmin";
-import TulimaAdminPanel from "./pages/admin";
-
-// 1. Importamos tu nuevo archivo de registro de proveedores
-// (Asumiendo que lo guardaste en la carpeta 'pages' como los demás)
 import RegistroProveedor from "./pages/registro_pro"; 
-
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import RutaProtegidaProveedor from "./components/RutaProtegidaProveedor";
+import Perfil from './pages/Perfil';
+import MFASetup from './pages/mfa-setup';
+import MFAVerify from './pages/mfa-verify';
+import Suscripcion from './pages/Suscripcion';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout><Home /></MainLayout>} />
-        <Route path="/hoteles" element={<MainLayout><Hoteles /></MainLayout>} />
-        <Route path="/restaurantes" element={<MainLayout><Restaurantes /></MainLayout>} />
-        <Route path="/tours" element={<MainLayout><Tours /></MainLayout>} />
-        <Route path="/login" element={<MainLayout><Login /></MainLayout>} />
-    
-        <Route path="/registro" element={<MainLayout><Registro /></MainLayout>} />
-        <Route
-          path="/admin"
-          element={
-            <RutaProtegidaAdmin>
-              <Admin />
-            </RutaProtegidaAdmin>
-          }
-        />
-        <Route
-          path="/dashboard-proveedor"
-          element={
-            <RutaProtegidaProveedor>
-              <DashboardProveedor />
-            </RutaProtegidaProveedor>
-          }
-        />
-        
-        <Route path="/registro" element={<MainLayout><Registro /></MainLayout>} />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="hoteles" element={<Hoteles />} />
+            <Route path="restaurantes" element={<Restaurantes />} />
+            <Route path="tours" element={<Tours />} />
+            <Route path="login" element={<Login />} />
+            <Route path="registro" element={<Registro />} />
+            <Route path="registro-proveedor" element={<RegistroProveedor />} />
+            <Route path="perfil" element={<Perfil />} />
+            <Route path="mfa-setup" element={<MFASetup />} />
+            <Route path="mfa-verify" element={<MFAVerify />} />
+            <Route path="suscripcion" element={<Suscripcion />} />
+          </Route>
 
-      
-        <Route path="/registro-proveedor" element={<MainLayout><RegistroProveedor /></MainLayout>} />
-        
-      </Routes>
+          <Route
+            path="/admin"
+            element={
+              <RutaProtegidaAdmin>
+                <Admin />
+              </RutaProtegidaAdmin>
+            }
+          />
+          <Route
+            path="/dashboard-proveedor"
+            element={
+              <RutaProtegidaProveedor>
+                <DashboardProveedor />
+              </RutaProtegidaProveedor>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
