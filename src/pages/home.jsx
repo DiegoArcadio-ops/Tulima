4eaef143b37e8e7025e5cef716da05c768bac048
Alan Parra <gohanEnd89@outlook.com>
2026-07-01 17:50:16 -0600

    modificacion visual y login

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
 import HeroSection from "../components/HeroSection"; 
 import FeaturedDestinations from "../components/FeaturedDestinations";
 import InteractiveMap from "../components/InteractiveMap";
@@ -6,6 +7,18 @@ import AboutColima from "../components/AboutColima";
 import './home.css';
 
 export default function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.scrollTo) {
+      // pequeño delay para que el DOM termine de montar
+      setTimeout(() => {
+        const el = document.getElementById(location.state.scrollTo);
+        if (el) el.scrollIntoView({ behavior: 'smooth' });
+      }, 100);
+    }
+  }, [location]);
+  
   return (
     <div className="home-page-container">
       <HeroSection />
